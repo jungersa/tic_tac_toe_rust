cec084a2b99135ec293096940aed7459ed98b0c0
Arnaud Jungers <arnaud.jungers@gmail.com>
2023-03-29 22:08:36 +0200

    Add Possibles Moves

--- src/logic/models.rs
+++ src/logic/models.rs
@@ -44,7 +44,7 @@ impl Cell {
     ///
     /// * `mark` - The mark which will be in the cell
     ///
-    pub fn new_used(mark: Mark) -> Self {
+    pub fn new_marked(mark: Mark) -> Self {
         Cell { mark: Some(mark) }
     }
 
@@ -293,7 +293,7 @@ impl GameState {
 
         let mut new_cells = [Cell::new_empty(); Grid::SIZE];
         new_cells[..cell_index].copy_from_slice(&self.grid.cells[..cell_index]);
-        new_cells[cell_index] = Cell::new_used(self.current_mark());
+        new_cells[cell_index] = Cell::new_marked(self.current_mark());
         new_cells[cell_index + 1..].copy_from_slice(&self.grid.cells[cell_index + 1..]);
 
         let new_grid = Grid::new(Some(new_cells));
@@ -305,6 +305,27 @@ impl GameState {
             before_state: *self,
             after_state: new_state,
         })
+    }
+
+    /// Returns a vector of all possible moves for the current state of the game.
+    ///
+    /// If the game is already over, returns an empty vector.
+    ///
+    /// # Returns
+    ///
+    /// A vector of `Move` structs, each representing a possible move in the game.
+    fn possible_moves(&self) -> Vec<Move> {
+        let mut moves: Vec<Move> = Vec::new();
+        if !self.game_over() {
+            self.grid.cells.iter().enumerate().for_each(|(i, cell)| {
+                if cell.is_vacant() {
+                    if let Ok(possible_move) = self.make_move_to(i) {
+                        moves.push(possible_move);
+                    }
+                }
+            })
+        }
+        moves
     }
 }
 
@@ -402,13 +423,13 @@ mod tests {
 
         #[test]
         fn test_new_occupied_cross() {
-            let cell = Cell::new_used(Mark::Cross);
+            let cell = Cell::new_marked(Mark::Cross);
             assert!(cell.is_occupied_by(Mark::Cross));
         }
 
         #[test]
         fn test_new_occupied_naught() {
-            let cell = Cell::new_used(Mark::Naught);
+            let cell = Cell::new_marked(Mark::Naught);
             assert!(cell.is_occupied_by(Mark::Naught));
         }
     }
@@ -427,15 +448,15 @@ mod tests {
         fn test_empty_count() {
             let grid = Grid {
                 cells: [
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
                 ],
             };
             assert_eq!(grid.empty_count(), 5);
@@ -445,15 +466,15 @@ mod tests {
         fn test_naught_count() {
             let grid = Grid {
                 cells: [
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
                 ],
             };
             assert_eq!(grid.naught_count(), 2);
@@ -463,15 +484,15 @@ mod tests {
         fn test_cross_count() {
             let grid = Grid {
                 cells: [
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
                     Cell::new_empty(),
                     Cell::new_empty(),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
                 ],
             };
             assert_eq!(grid.cross_count(), 4);
@@ -480,15 +501,15 @@ mod tests {
         #[test]
         fn test_new_with_cells() {
             let cells = [
-                Cell::new_used(Mark::Cross),
-                Cell::new_used(Mark::Naught),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
                 Cell::new_empty(),
-                Cell::new_used(Mark::Cross),
-                Cell::new_empty(),
-                Cell::new_empty(),
+                Cell::new_marked(Mark::Cross),
                 Cell::new_empty(),
                 Cell::new_empty(),
-                Cell::new_used(Mark::Naught),
+                Cell::new_empty(),
+                Cell::new_empty(),
+                Cell::new_marked(Mark::Naught),
             ];
             let grid = Grid::new(Some(cells));
 
@@ -553,14 +574,14 @@ mod tests {
         #[test]
         fn test_current_mark_starting_mark_cross_one_move() {
             let mut game_state = GameState::new(Grid::new(None), Some(Mark::Cross));
-            game_state.grid.cells[0] = Cell::new_used(Mark::Cross);
+            game_state.grid.cells[0] = Cell::new_marked(Mark::Cross);
             assert_eq!(game_state.current_mark(), Mark::Naught);
         }
 
         #[test]
         fn test_current_mark_starting_mark_naught_one_move() {
             let mut game_state = GameState::new(Grid::new(None), Some(Mark::Naught));
-            game_state.grid.cells[0] = Cell::new_used(Mark::Naught);
+            game_state.grid.cells[0] = Cell::new_marked(Mark::Naught);
             assert_eq!(game_state.current_mark(), Mark::Cross);
         }
 
@@ -574,9 +595,9 @@ mod tests {
         #[test]
         fn test_winner_mark_row() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[0] = Cell::new_used(Mark::Cross);
-            cells[1] = Cell::new_used(Mark::Cross);
-            cells[2] = Cell::new_used(Mark::Cross);
+            cells[0] = Cell::new_marked(Mark::Cross);
+            cells[1] = Cell::new_marked(Mark::Cross);
+            cells[2] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winner_mark(), Some(Mark::Cross));
@@ -585,9 +606,9 @@ mod tests {
         #[test]
         fn test_winner_mark_column() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[0] = Cell::new_used(Mark::Cross);
-            cells[3] = Cell::new_used(Mark::Cross);
-            cells[6] = Cell::new_used(Mark::Cross);
+            cells[0] = Cell::new_marked(Mark::Cross);
+            cells[3] = Cell::new_marked(Mark::Cross);
+            cells[6] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winner_mark(), Some(Mark::Cross));
@@ -596,9 +617,9 @@ mod tests {
         #[test]
         fn test_winner_mark_diagonal() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[0] = Cell::new_used(Mark::Cross);
-            cells[4] = Cell::new_used(Mark::Cross);
-            cells[8] = Cell::new_used(Mark::Cross);
+            cells[0] = Cell::new_marked(Mark::Cross);
+            cells[4] = Cell::new_marked(Mark::Cross);
+            cells[8] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winner_mark(), Some(Mark::Cross));
@@ -607,9 +628,9 @@ mod tests {
         #[test]
         fn test_winner_mark_false() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[1] = Cell::new_used(Mark::Cross);
-            cells[4] = Cell::new_used(Mark::Cross);
-            cells[8] = Cell::new_used(Mark::Cross);
+            cells[1] = Cell::new_marked(Mark::Cross);
+            cells[4] = Cell::new_marked(Mark::Cross);
+            cells[8] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winner_mark(), None);
@@ -625,9 +646,9 @@ mod tests {
         #[test]
         fn test_winner_cells_row() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[0] = Cell::new_used(Mark::Cross);
-            cells[1] = Cell::new_used(Mark::Cross);
-            cells[2] = Cell::new_used(Mark::Cross);
+            cells[0] = Cell::new_marked(Mark::Cross);
+            cells[1] = Cell::new_marked(Mark::Cross);
+            cells[2] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winning_indexes(), Some(vec![0, 1, 2]));
@@ -636,9 +657,9 @@ mod tests {
         #[test]
         fn test_winner_cells_column() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[0] = Cell::new_used(Mark::Cross);
-            cells[3] = Cell::new_used(Mark::Cross);
-            cells[6] = Cell::new_used(Mark::Cross);
+            cells[0] = Cell::new_marked(Mark::Cross);
+            cells[3] = Cell::new_marked(Mark::Cross);
+            cells[6] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winning_indexes(), Some(vec![0, 3, 6]));
@@ -647,9 +668,9 @@ mod tests {
         #[test]
         fn test_winner_cells_diagonal() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[0] = Cell::new_used(Mark::Cross);
-            cells[4] = Cell::new_used(Mark::Cross);
-            cells[8] = Cell::new_used(Mark::Cross);
+            cells[0] = Cell::new_marked(Mark::Cross);
+            cells[4] = Cell::new_marked(Mark::Cross);
+            cells[8] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winning_indexes(), Some(vec![0, 4, 8]));
@@ -658,9 +679,9 @@ mod tests {
         #[test]
         fn test_winner_cells_false() {
             let mut cells = [Cell::new_empty(); Grid::SIZE];
-            cells[1] = Cell::new_used(Mark::Cross);
-            cells[4] = Cell::new_used(Mark::Cross);
-            cells[8] = Cell::new_used(Mark::Cross);
+            cells[1] = Cell::new_marked(Mark::Cross);
+            cells[4] = Cell::new_marked(Mark::Cross);
+            cells[8] = Cell::new_marked(Mark::Cross);
             let grid = Grid::new(Some(cells));
             let game_state = GameState::new(grid, None);
             assert_eq!(game_state.winning_indexes(), None);
@@ -671,7 +692,7 @@ mod tests {
             let empty_game = GameState::new(Grid::new(None), None);
             let non_empty_game = GameState::new(
                 Grid::new(Some([
-                    Cell::new_used(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
@@ -693,23 +714,23 @@ mod tests {
             let empty_game = GameState::new(Grid::new(None), None);
             let tie_game = GameState::new(
                 Grid::new(Some([
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
                 ])),
                 None,
             );
             let cross_wins_game = GameState::new(
                 Grid::new(Some([
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
@@ -721,15 +742,15 @@ mod tests {
             );
             let naught_wins_game = GameState::new(
                 Grid::new(Some([
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
                     Cell::new_empty(),
                     Cell::new_empty(),
                     Cell::new_empty(),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Naught),
                 ])),
                 None,
             );
@@ -745,15 +766,15 @@ mod tests {
             let empty_game = GameState::new(Grid::new(None), None);
             let non_empty_game = GameState::new(
                 Grid::new(Some([
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
-                    Cell::new_used(Mark::Cross),
-                    Cell::new_used(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
+                    Cell::new_marked(Mark::Cross),
+                    Cell::new_marked(Mark::Naught),
                 ])),
                 None,
             );
@@ -772,19 +793,19 @@ mod tests {
             assert_eq!(mv.cell_index, 0);
             assert_eq!(mv.before_state, game);
             assert_eq!(mv.after_state.starting_mark, game.starting_mark);
-            assert_eq!(mv.after_state.grid.cells[0], Cell::new_used(Mark::Cross));
+            assert_eq!(mv.after_state.grid.cells[0], Cell::new_marked(Mark::Cross));
         }
 
         #[test]
         fn test_make_move_to_occupied_cell() {
             let cells = [
-                Cell::new_used(Mark::Cross),
+                Cell::new_marked(Mark::Cross),
                 Cell::new_empty(),
                 Cell::new_empty(),
-                Cell::new_used(Mark::Naught),
+                Cell::new_marked(Mark::Naught),
                 Cell::new_empty(),
-                Cell::new_used(Mark::Cross),
-                Cell::new_used(Mark::Naught),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
                 Cell::new_empty(),
                 Cell::new_empty(),
             ];
@@ -793,6 +814,49 @@ mod tests {
             assert!(result.is_err());
             let err = result.unwrap_err();
             assert_eq!(err, "Cell is not empty");
+        }
+
+        #[test]
+        fn test_possible_moves_empty_game() {
+            let game = GameState::new(Grid::new(None), None);
+            let moves = game.possible_moves();
+            assert_eq!(moves.len(), 9);
+        }
+
+        #[test]
+        fn test_possible_moves_game_in_progress() {
+            let grid = Grid::new(Some([
+                Cell::new_empty(),
+                Cell::new_empty(),
+                Cell::new_empty(),
+                Cell::new_empty(),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
+                Cell::new_empty(),
+            ]));
+            let game = GameState::new(grid, Some(Mark::Cross));
+            let moves = game.possible_moves();
+            assert_eq!(moves.len(), 5);
+        }
+
+        #[test]
+        fn test_possible_moves_game_over() {
+            let grid = Grid::new(Some([
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
+                Cell::new_marked(Mark::Naught),
+                Cell::new_marked(Mark::Cross),
+                Cell::new_marked(Mark::Naught),
+            ]));
+            let game = GameState::new(grid, Some(Mark::Cross));
+            let moves = game.possible_moves();
+            assert!(moves.is_empty());
         }
     }
 }
