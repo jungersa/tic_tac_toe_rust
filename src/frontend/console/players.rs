98f677f96b29ac5d47c3b9349f4a7b740869a00c
Arnaud Jungers <arnaud.jungers@gmail.com>
2023-04-03 23:05:15 +0200

    feat: :sparkles: Add the coord_to_index in the player cli

--- src/frontend/console/players.rs
+++ src/frontend/console/players.rs
@@ -2,6 +2,8 @@
 
 use std::io;
 
+use clap::builder::NonEmptyStringValueParser;
+
 use crate::{
     game::players::Player,
     logic::{GameMove, GameState, Mark},
@@ -34,8 +36,8 @@ impl Player for ConsolePlayer {
                 .read_line(&mut input_string)
                 .expect("Failed to read input.");
 
-            match input_string.trim().parse() {
-                Ok(input) => {
+            match coord_to_index(input_string.trim()) {
+                Some(input) => {
                     if (0..9).contains(&input) {
                         if let Ok(next_move) = game_state.make_move_to(input) {
                             return Some(next_move);
@@ -45,7 +47,7 @@ impl Player for ConsolePlayer {
                         println!("Invalid input. Try again.");
                     }
                 }
-                Err(_) => {
+                None => {
                     println!("Invalid input. Try again.");
                 }
             }
@@ -56,4 +58,31 @@ impl Player for ConsolePlayer {
     fn get_mark(&self) -> Mark {
         self.mark
     }
+}
+
+
+fn coord_to_index(coord: &str) -> Option<usize> {
+    let chars: Vec<char> = coord.chars().collect();
+    if chars.len() != 2 {
+        return None;
+    }
+    let row = match chars[0] {
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        '1' => 0,
+        '2' => 1,
+        '3' => 2,
+        _ => return None,
+    };
+    let col = match chars[1] {
+        '1' => 0,
+        '2' => 1,
+        '3' => 2,
+        'A' => 0,
+        'B' => 1,
+        'C' => 2,
+        _ => return None,
+    };
+    Some(row * 3 + col)
 }
