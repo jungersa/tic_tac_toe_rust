5c2b948da891afb7420acfd40083a1d90df85eb6
Arnaud Jungers <arnaud.jungers@gmail.com>
2023-04-04 23:14:01 +0200

    perf: :zap: Add Alpha Beta prunning to minimax player

--- src/game/players/minimax.rs
+++ src/game/players/minimax.rs
@@ -41,10 +41,13 @@ impl Player for MinimaxPlayer {
 /// * `game_state` - The game state to find the best move for.
 fn find_best_move(game_state: &GameState) -> Option<GameMove> {
     let maximized_player = game_state.current_mark();
+    let alpha = i32::MIN;
+    let beta = i32::MAX;
+
     game_state
         .possible_moves()
         .into_iter()
-        .max_by_key(|move_| minimax(move_, maximized_player, false))
+        .max_by_key(|move_| minimax_with_pruning(move_, maximized_player, false, alpha, beta))
 }
 
 /// Finds the score of the given move.
@@ -58,6 +61,7 @@ fn find_best_move(game_state: &GameState) -> Option<GameMove> {
 /// * `move_` - The move to find the score of.
 /// * `maximized_player` - The maximized player.
 /// * `choose_highest_score` - Whether to choose the highest score or the lowest score.
+#[allow(dead_code)]
 fn minimax(move_: &GameMove, maximized_player: Mark, choose_highest_score: bool) -> i32 {
     if move_.after_state().game_over() {
         return move_.after_state().score(maximized_player).unwrap();
@@ -72,4 +76,63 @@ fn minimax(move_: &GameMove, maximized_player: Mark, choose_highest_score: bool)
     } else {
         scores.min().unwrap()
     }
+}
+
+/// Finds the score of the given move.
+/// The score is the score of the after_state of the move.
+/// If the after_state is not a game over state, the score is the score of the best move for the other player.
+/// The best move for the other player is the move with the highest score if the maximized player is the other player.
+/// The best move for the other player is the move with the lowest score if the maximized player is the maximized player.
+/// Use alpha-beta pruning to speed up the algorithm.
+///
+/// # Arguments
+///
+/// * `move_` - The move to find the score of.
+/// * `maximized_player` - The maximized player.
+/// * `choose_highest_score` - Whether to choose the highest score or the lowest score.
+/// * `alpha` - The alpha value.
+/// * `beta` - The beta value.
+fn minimax_with_pruning(
+    move_: &GameMove,
+    maximized_player: Mark,
+    choose_highest_score: bool,
+    alpha: i32,
+    beta: i32,
+) -> i32 {
+    if move_.after_state().game_over() {
+        return move_.after_state().score(maximized_player).unwrap();
+    }
+
+    let mut best_score = if choose_highest_score {
+        i32::MIN
+    } else {
+        i32::MAX
+    };
+
+    let mut new_alpha = alpha;
+    let mut new_beta = beta;
+
+    for child_move in move_.after_state().possible_moves() {
+        let score = minimax_with_pruning(
+            &child_move,
+            maximized_player,
+            !choose_highest_score,
+            new_alpha,
+            new_beta,
+        );
+
+        if choose_highest_score {
+            best_score = best_score.max(score);
+            new_alpha = new_alpha.max(score);
+        } else {
+            best_score = best_score.min(score);
+            new_beta = new_beta.min(score);
+        }
+
+        if beta <= alpha {
+            break; // alpha-beta pruning
+        }
+    }
+
+    best_score
 }
